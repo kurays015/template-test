df8f6c207193539d797e2f1e2e9adc7e1fbff368
Christzxz <126612073+kurays015@users.noreply.github.com>
2026-02-24 21:49:33 +0800

    feat: update minikit.config.ts with account association

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,29 +1,29 @@
-const ROOT_URL = "https://template-test-chi-five.vercel.app/";
+const ROOT_URL = "https://www.youtube.com/watch?v=_k22WAEAfpE";
 
 export const minikitConfig = {
   accountAssociation: {
-    header: "eyJmaWQiOjIzNzk2NzMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhCM0M5OEU5RWY0M2Y3ODJEZWIwOTVjMGM1NTcyRjZCNjQ4QzZENDExIn0",
-    payload: "eyJkb21haW4iOiJ0ZW1wbGF0ZS10ZXN0LWNoaS1maXZlLnZlcmNlbC5hcHAifQ",
-    signature: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEH1flkI3lkIOwf0ZNNh_szdgu2VdXh3QjaryJpFwy3W7Suc9MUEsvpZsW_BzGkm7IPOSFIh6QIkwbzqX8rul_SAGwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    header: "asd",
+    payload: "adad",
+    signature: "adad",
   },
   miniapp: {
     version: "1",
     name: "My Base Mini-App",
     subtitle: "A Base Mini-App",
     description: "Built with Base Builder",
-    screenshotUrls: ["https://template-test-chi-five.vercel.app//image.png"],
-    iconUrl: "https://template-test-chi-five.vercel.app/image.png",
-    splashImageUrl: "https://template-test-chi-five.vercel.app/image.png",
-    splashBackgroundColor: "#a34d4d",
+    screenshotUrls: ["https://www.youtube.com/watch?v=_k22WAEAfpE/image.png"],
+    iconUrl: "https://www.youtube.com/watch?v=_k22WAEAfpE/image.png",
+    splashImageUrl: "https://www.youtube.com/watch?v=_k22WAEAfpE/image.png",
+    splashBackgroundColor: "#000000",
     homeUrl: ROOT_URL,
-    webhookUrl: "https://template-test-chi-five.vercel.app/api/webhook",
-    primaryCategory: "utility",
+    webhookUrl: "https://www.youtube.com/watch?v=_k22WAEAfpE/api/webhook",
+    primaryCategory: "social",
     tags: ["base", "miniapp"],
-    heroImageUrl: "https://template-test-chi-five.vercel.app/image.png",
-    tagline: "Save instantly",
-    ogTitle: "My Base Mini-Appasd",
-    ogDescription: "Built with Base Builderasdad",
-    ogImageUrl: "https://template-test-chi-five.vercel.app/image.png",
+    heroImageUrl: "https://www.youtube.com/watch?v=_k22WAEAfpE/image.png",
+    tagline: "asdasda",
+    ogTitle: "My Base Mini-App",
+    ogDescription: "Built with Base Builder",
+    ogImageUrl: "https://www.youtube.com/watch?v=_k22WAEAfpE/image.png",
     "noindex": true
 
   },
